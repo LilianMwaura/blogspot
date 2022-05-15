797e598da4ca420e7d48c4f27db36aa536b8fbf6
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-15 15:31:32 +0300

    create manage.py

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wambui:db_password@localhost:5432/pitchbank'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    DEBUG = True
+
+class ProdConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)  
+    ...
+    
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wambui:db_password@localhost:5432/pitchbank'
+    SECRET_KEY = 'thisismysecret'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+} 
