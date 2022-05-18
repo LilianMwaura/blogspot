5aeb7e89e34353a369c2727d06d875a673637287
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-18 10:34:24 +0300

    add configurations

--- config.py
+++ config.py
@@ -2,17 +2,17 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wambui:db_password@localhost:5432/pitchbank'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wambui:db_password@localhost:5432/blog'
     SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG = True
 
 class ProdConfig(Config):
     # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)  
-    ...
+    pass
     
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wambui:db_password@localhost:5432/pitchbank'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wambui:db_password@localhost:5432/blog'
     SECRET_KEY = 'thisismysecret'
     DEBUG = True
 
