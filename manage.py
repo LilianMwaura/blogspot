eb12dbd4c920449b145c62f8dfee535089f4c884
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-18 11:10:04 +0300

    update migrations

--- manage.py
+++ manage.py
@@ -1,11 +1,10 @@
+from flask_migrate import Migrate, MigrateCommand
 from app import create_app,db
 from flask_script import Manager, Server
-from flask_migrate import Migrate, MigrateCommand
-from app.models import *
 
 
 # Creating app instance
-app = create_app('production')
+app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
